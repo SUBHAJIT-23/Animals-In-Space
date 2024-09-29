6ac23fc46bc309db74f83477571dcd0c1f6d19af
SUBHAJIT-23 <subhajitkarmakar432@gmail.com>
2024-09-29 12:00:40 +0530

    Update main.js

--- Javascript/main.js
+++ Javascript/main.js
@@ -7,3 +7,26 @@ function tg()
 {
     window.location.href = "https://en.wikipedia.org/wiki/Tiger"
 }
+
+const contele = document.querySelector(".container");
+const txt = ["Animals In Space"];
+let ci = 0;
+let chri = 0;
+
+updateText();
+
+function updateText()
+{
+  chri++;
+  contele.innerHTML = `<h1>${txt[ci].slice(0, chri)}</h1>`;
+  if (chri === txt[ci].length)
+  {
+    ci++;
+    chri = 0;
+  }
+  if (ci === txt.length)
+  {
+    ci = 0;
+  }
+  setTimeout(updateText, 400);
+}
